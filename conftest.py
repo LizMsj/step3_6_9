18959d0be074753801df3371a14d34d135096ff6
LizMsj <Moysey9g@mail/ru>
2022-03-26 21:14:17 +0300

    проверка наличия кнопки "добавить в корзину"

--- conftest.py
+++ conftest.py
@@ -1,17 +1,26 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='ru',
-                     help="Choose language: ru or es")
+    parser.addoption('--language', action='store', default='es',
+                     help="Choose language: es, ru and else")
+    parser.addoption('--browser_name', action='store', default='Chrome',
+                     help="Choose browser: Firefox or Chrome")
 
-@pytest.fixture.language(scope="function")
-def language():
-    pass
 
-@pytest.fixture.browser(scope="function", )
-def browser():
-    browser = webdriver.Chrome()
+@pytest.fixture(scope='function')
+def browser(request):
+    user_language = request.config.getoption('language')
+    user_browser = request.config.getoption('browser_name')
+    if user_browser == 'Chrome':
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif user_browser == 'Firefox':
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     yield browser
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
-import pytest
+from selenium.webdriver.common.by import By
+from time import sleep
 
 
-def test_check_button(browser):
+def test_check_button_on_page(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    button = browser.find_element_by_css_selector('.btn.btn-lg.btn-primary')\
-    assert button, "Нет кнопки 'добавить в корзину'"
+    button = browser.find_elements(By.CSS_SELECTOR, '.btn-primary')
+    sleep(5)
+    assert button, 'basket button not found'
